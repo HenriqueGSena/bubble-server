e2b5838ae923f3be8e2944243173dbec0fd116b2
henrique.sena <carlos.sena@pineapples.com.br>
2025-04-03 18:56:19 -0300

    feat: Busca por lista de apartamentos
    - Configurando arquivo service prisma para realizar query de busca por apartamento no banco
    - Configurando app.service para enviar resultado da lista para controller

---
+++ src/utils/accommodation.ts
@@ -0,0 +1,10 @@
+export interface Accommodation {
+    id_acc: string,
+    nome_acomodacao: string,
+    cama: string[],
+    qtde_quartos: number,
+    qtde_banheiros: string,
+    cidade: string,
+    endereco: string,
+    bairro: string
+}
