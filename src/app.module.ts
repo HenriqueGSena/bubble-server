4556e867596adda071522a3caab8b88665845d4b
henrique.sena <carlos.sena@pineapples.com.br>
2025-04-03 17:44:14 -0300

    feat: Configurando server prisma
    - Configurando arquivo service para conexao com banco de dados
    - Incluindo service no module app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaServer } from './db/prisma.server';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaServer],
 })
 export class AppModule {}
