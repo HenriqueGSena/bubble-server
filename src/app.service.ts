3fc0ffb65af2d48ee192e6dd9d2438520181cd84
henrique.sena <carlos.sena@pineapples.com.br>
2025-04-07 15:46:46 -0300

    Ajuste no retorno dos dados via api

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,15 +4,30 @@ import { PrismaServer } from './db/prisma.server';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly prismaService: PrismaServer) { }
 
-  constructor(private readonly prismaService: PrismaServer) {}
-  
   public async findAccommodation(): Promise<Accommodation[]> {
     try {
-      const db = await this.prismaService.findListAccommodation() as Accommodation[];
-      return db;
+      const db = await this.prismaService.findListAccommodation() as any[];
+
+      const acomodacoes: Accommodation[] = db.map((item) => {
+        let camaObj = {};
+
+        try {
+          camaObj = JSON.parse(item.cama.replace(/'/g, '"'));
+        } catch (e) {
+          console.warn(`Erro ao parsear cama da acomodação ${item.id_acc}`, e);
+        }
+
+        return {
+          ...item,
+          cama: camaObj,
+        };
+      });
+
+      return acomodacoes;
     } catch (e) {
-      console.error('Error na busca pelas acomodações', e);
+      console.error('Erro na busca pelas acomodações', e);
       throw e;
     }
   }
--- src/utils/accommodation.ts
+++ src/utils/accommodation.ts
@@ -1,10 +1,17 @@
 export interface Accommodation {
     id_acc: string,
     nome_acomodacao: string,
-    cama: string[],
+    cama: Cama,
     qtde_quartos: number,
     qtde_banheiros: string,
     cidade: string,
     endereco: string,
     bairro: string
+}
+
+interface Cama {
+    DOUBLE?: number;
+    QUEENSIZE?: number;
+    INDIVIDUAL?: number;
+    [tipoOutro: string]: number | undefined;
 }
